6ded31d2eaefb749bd9497dcdbaf8d0a95de3605
Colas <colas@loeilduchat.fr>
2026-05-13 15:04:24 +0200

    "Aircraft Carrier" Asset

---
+++ Assets/01_Scripts/03_Managers/CameraManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _cameras;
+
+    private void Start()
+    {
+        if (_cameras.Length > 0)
+        {
+            ActivateCamera(0);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1)) ActivateCamera(0);
+        if (Input.GetKeyDown(KeyCode.F2)) ActivateCamera(1);
+        if (Input.GetKeyDown(KeyCode.F3)) ActivateCamera(2);
+    }
+
+    private void ActivateCamera(int targetIndex)
+    {
+        for (int i = 0; i < _cameras.Length; i++)
+        {
+            if (_cameras[i] != null)
+            {
+                _cameras[i].SetActive(i == targetIndex);
+            }
+        }
+    }
+}
